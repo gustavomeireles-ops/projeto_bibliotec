008e5042c621d9c70530654a3b02433ce6987f4c
GUSTAVO CASTILHO DE MEIRELES <57034439893@info.local>
2026-09-08 17:27:18 -0300

    :construction: build: Página de listagem em andamento, menu lateral adcionado

--- login/login.js
+++ login/login.js
@@ -1,8 +1,17 @@
 const campoSenha = document.getElementById("senha");
 const btnSenha = document.querySelector("#mostrar-senha");
 console.log(campoSenha); 
+console.log(campoSenha.type); 
 console.log(btnSenha);
 
 btnSenha.addEventListener("click", function(){
-    alert("Cliquei no botão!");
+    // if(campoSenha.type == "password"){
+    //     campoSenha.type = "text";
+    // }
+    // else{
+    //     campoSenha.type = "password";
+    // }
+    campoSenha.type = campoSenha.type == "password" ? "texto" : "password"
 })
+
+
