31dd9b62dca090f5aec4df2ad78d8b270e127479
GUSTAVO CASTILHO DE MEIRELES <57034439893@info.local>
2026-09-08 14:24:04 -0300

    :construction: build: Página de login em andamento

---
+++ login/login.js
@@ -0,0 +1,8 @@
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+console.log(campoSenha); 
+console.log(btnSenha);
+
+btnSenha.addEventListener("click", function(){
+    alert("Cliquei no botão!");
+})
